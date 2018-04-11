2de6db9fccdd200a1861ca60cf644c477dd8dc5a
Cassidy James Blaede <c@ssidyjam.es>
2018-04-11 17:48:03 -0600

    Update to some sane defaults

--- web/views/shop_view.ex
+++ web/views/shop_view.ex
@@ -2,16 +2,20 @@ defmodule Deckard.ShopView do
   def render("projects.json", _assigns) do
     %{
       data: [
-        "com.github.cassidyjames.palette",
-        "com.github.plugarut.pwned-checker",
-        "com.github.artemanufrij.findfileconflicts",
-        "com.github.santileortiz.iconoscope",
-        "com.github.bleakgrey.transporter",
-        "com.github.bartzaalberg.alias",
-        "com.github.artemanufrij.graphui",
-        "com.github.peteruithoven.resizer",
-        "com.github.bartzaalberg.snaptastic",
-        "com.github.cjfloss.envelope"
+        "chromium-browser",
+        "meld",
+        "telegramdesktop",
+        "org.gnome.Builder",
+        "steam",
+        "virtualbox",
+        "virt-manager",
+        "glade",
+        "vlc",
+        "org.kde.krita",
+        "gimp",
+        "inkscape",
+        "keepassx",
+        "scribus"
       ]
     }
   end
@@ -19,16 +23,6 @@ defmodule Deckard.ShopView do
   def render("releases.json", _assigns) do
     %{
       data: [
-        "com.github.cassidyjames.palette",
-        "com.github.plugarut.pwned-checker",
-        "com.github.artemanufrij.findfileconflicts",
-        "com.github.santileortiz.iconoscope",
-        "com.github.bleakgrey.transporter",
-        "com.github.bartzaalberg.alias",
-        "com.github.artemanufrij.graphui",
-        "com.github.peteruithoven.resizer",
-        "com.github.bartzaalberg.snaptastic",
-        "com.github.cjfloss.envelope"
       ]
     }
   end
@@ -36,16 +30,6 @@ defmodule Deckard.ShopView do
   def render("downloads.json", _assigns) do
     %{
       data: [
-        "com.github.cassidyjames.palette",
-        "com.github.plugarut.pwned-checker",
-        "com.github.artemanufrij.findfileconflicts",
-        "com.github.santileortiz.iconoscope",
-        "com.github.bleakgrey.transporter",
-        "com.github.bartzaalberg.alias",
-        "com.github.artemanufrij.graphui",
-        "com.github.peteruithoven.resizer",
-        "com.github.bartzaalberg.snaptastic",
-        "com.github.cjfloss.envelope"
       ]
     }
   end
