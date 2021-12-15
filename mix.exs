0e48a428ed83ab538cd3e8c72eea425112860af6
Dairon Medina Caro <dairon@system76.com>
2021-12-15 12:01:03 -0500

    Upgrade all the elixir packages and remove outdated Redis packages

--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule Deckard.Mixfile do
       app: :deckard,
       version: "0.1.0",
       elixir: "~> 1.5",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -24,28 +24,23 @@ defmodule Deckard.Mixfile do
 
   # Specifies which paths to compile per environment
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # Runtime dependencies
-      {:cors_plug,            "~> 1.1"},
-      {:cowboy,               "~> 1.0"},
-      {:exredis,              ">= 0.2.4"},
-      {:httpoison,            "~> 0.13"},
-      {:phoenix,              "~> 1.3.2"},
-
+      {:cors_plug, "~> 2.0"},
+      {:plug_cowboy, "~> 2.5"},
+      {:redix, "~> 1.0.0"},
+      {:jason, "~> 1.0"},
+      {:phoenix, "~> 1.6"},
       # Development and testing dependencies
-      {:credo,                "~> 0.8", only: [:dev, :test], runtime: false},
-      {:dialyxir,             "~> 0.5.0", only: [:dev, :test], runtime: false},
-      {:ex_doc,               "~> 0.14", only: :dev, runtime: false},
-      {:mix_test_watch,       "~> 0.3", only: :dev, runtime: false},
-
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       # Deployment dependencies
-      {:distillery,           "~> 2.0.12"},
-      {:edeliver,             "~> 1.6.0"},
-      {:toml,                 "~> 0.5.2"},
+      {:toml, "~> 0.6.2"}
     ]
   end
 
@@ -56,14 +51,6 @@ defmodule Deckard.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    [
-      deploy: &deploy/1,
-    ]
-  end
-
-  defp deploy([env]) do
-    Mix.Task.run("edeliver", ["upgrade", env])
-    Mix.Task.reenable("edeliver")
-    Mix.Task.run("edeliver", ["migrate", env])
+    []
   end
 end
